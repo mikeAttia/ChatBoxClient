d9eab29f68969b2543e86be937e8231ba1c82f60
Michael <dr.mike.attia@gmail.com>
2017-02-05 11:44:35 +0200

    Sign in and sign Up scenes only ... switching between them ... no other functionality

--- src/chatboxclient/ChatBoxClient.java
+++ src/chatboxclient/ChatBoxClient.java
@@ -19,7 +19,7 @@ public class ChatBoxClient extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("ChatBoxClientFXMLDoc.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("SignInViewFXMLDoc.fxml"));
         
         Scene scene = new Scene(root);
         
--- src/chatboxclient/ChatBoxClientFXMLDocController.java
+++ src/chatboxclient/ChatBoxClientFXMLDocController.java
@@ -5,12 +5,19 @@
  */
 package chatboxclient;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 /**
  *
@@ -20,6 +27,44 @@ public class ChatBoxClientFXMLDocController implements Initializable {
     
     @FXML
     private Label label;
+    
+    @FXML
+    public Hyperlink goToSignUp; 
+    
+    @FXML
+    private Hyperlink backToSignIn;
+    
+    @FXML
+    private Stage stage;
+    
+    @FXML
+    private void goToSignUp(ActionEvent event) throws IOException
+    {
+        
+        Node node=(Node) event.getSource();
+        stage=(Stage) node.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("SignUpViewFXMLDoc.fxml"));/* Exception */
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+  
+  
+    }
+    
+    @FXML
+    private void backToSignIn(ActionEvent event) throws IOException
+    {
+        
+        Node node=(Node) event.getSource();
+        stage=(Stage) node.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("SignInViewFXMLDoc.fxml"));/* Exception */
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+  
+  
+    }
+    
     
     @FXML
     private void handleButtonAction(ActionEvent event) {
